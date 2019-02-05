b3415bdaed14e6885e21fdbf114d854e65a29dfa
joaogularte <jov96@hotmail.com>
2019-02-05 10:59:43 -0200

    Completed GitHubApi.js

--- src/external_api/GitHubApi.js
+++ src/external_api/GitHubApi.js
@@ -5,24 +5,24 @@ const axios = require('axios');
 
 class GitHubApi {
   /**
-     * Return a list all users from GitHub,  in the order that they signed up on GitHub.
-     */
+   * Return a list all users from GitHub,  in the order that they signed up on GitHub.
+   */
   static list(n) {
     const url = `https://api.github.com/users?since=${n}`;
     return axios.get(url);
   }
 
   /**
-     * Return publicly available information about someone with GitHub account
-     */
+   * Return publicly available information about someone with GitHub account
+   */
   static getUser(username) {
     const url = `https://api.github.com/users/${username}`;
     return axios.get(url);
   }
 
   /**
-    * Return a list public repositories for the specified user.
-    */
+   * Return a list public repositories for the specified user.
+   */
   static getUserRepo(username) {
     const url = `https://api.github.com/users/${username}/repos`;
     return axios.get(url);
