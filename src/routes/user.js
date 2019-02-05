3ee77a4c5840c82026ce468833950773c06907d5
joaogularte <jov96@hotmail.com>
2019-02-05 13:40:11 -0200

    Create user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,19 @@
 const express = require('express');
 
+/* Relative dependencies  */
+const UserSchema = require('./schemas/UserSchema');
+const UserController = require('../controllers/UserController');
+const schemaValidator = require('../middlewares/schemaValidator');
+
 const router = express.Router();
+
+/* GET /api/users */
+router.get('/', schemaValidator(UserSchema, 'list'), UserController.list);
+
+/* GET /api/users/:username/details */
+router.get('/:username/details', schemaValidator(UserSchema, 'get'), UserController.getUser);
+
+/* GET /api/users/:username/repos */
+router.get('/:username/repos'.schemaValidator(UserSchema, 'get'), UserController.getUserRepo);
 
 module.exports = router;
