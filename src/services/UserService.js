33b11d5edea938eee3fe45229e829bca1a1e955e
joaogularte <jov96@hotmail.com>
2019-02-05 01:04:25 -0200

    ;)

---
+++ src/services/UserService.js
@@ -0,0 +1,29 @@
+const GitHubApi = require('../external_api/GitHubApi');
+
+class UserService {
+  /**
+     * Return a list of users from GitHub
+     */
+  static async list(number) {
+    const users = await GitHubApi.list(number);
+    return users;
+  }
+
+  /**
+    * Return information about a specified user
+    */
+  static async getUser(username) {
+    const user = await GitHubApi.getUser(username);
+    return user;
+  }
+
+  /**
+    * Return a list public repositories for the specified user
+    */
+  static async getUserRepo(username) {
+    const userRepos = await GitHubApi.getUserRepo(username);
+    return userRepos;
+  }
+}
+
+module.exports = UserService;
